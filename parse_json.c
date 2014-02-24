403eeb39f811d8fd4f057fced2fdd1cfe150ec9a
goodian <g.goodian@gmail.com>
2014-02-23 22:31:17 -0800

    update

--- parse_json.c
+++ parse_json.c
@@ -4,11 +4,14 @@
 typedef unsigned int size_t;
 
 enum state {
-    begin, 
-    k_begin,
+    begin,        //key/value pair begin
+    k_begin_s_d,  //begin with "
+    k_begin_s_s,  //begin with '
+    k_begin_n,    //begin with no " nor '
     k_end,
-    v_begin_s, //以引号开始的alue
-    v_begin_n, //以数字形式开始的alue
+    v_begin_s_d, //begin with "
+    v_begin_s_s, //begin with '
+    v_begin_n,   //begin with no " nor '
     v_end,
     done
 };
@@ -28,9 +31,9 @@ static inline int http_json_arg_decode(char *session, char *s, const size_t len)
         s++;                                        
 
     for (; pos < e; pos++) {                        
-        switch (*pos) {                              
+        switch (*pos) { 
         case ' ':
-            break;
+			break;
         case '{':
             //if (e_stat == v_begin) {
             if (e_stat == k_end) {
@@ -39,77 +42,118 @@ static inline int http_json_arg_decode(char *session, char *s, const size_t len)
                 r_braces = find_right_braces(pos, e - pos);
                 v_len = r_braces + 1;
                 e_stat = v_end;
-                ret = http_json_arg_decode(session, pos, r_braces);
+                ret = http_json_arg_decode(session, pos, v_len);
                 pos = v + v_len - 1;
-                printf ("ret is %d.\n", ret);
+                //printf ("\npos is %.*s.\n", v_len, pos);
+                //printf ("ret is %d.\n", ret);
                 if (ret != 0)
                     goto end_with_error;
-            }
+            } 
+			//
             break;
         case ':':
-            if (e_stat == begin || e_stat == k_begin || e_stat == v_end)
-                goto end_with_error;
+			if (e_stat == k_begin_n) {
+				e_stat = k_end;
+				k_len = pos - k;
+			}
+			//
             break;                                  
         case '\"':                                  
-            if (e_stat == begin || e_stat == v_end) {
-                e_stat = k_begin;
-                k = pos + 1;
-            } else if (e_stat == k_begin) {
-                k_len = pos - k;
-                e_stat = k_end;
-            } else if (e_stat == k_end) {
-                v = pos + 1;
-                e_stat = v_begin_s;
-            } else if (e_stat == v_begin_s) {
-                v_len = pos - v;
-                e_stat = v_end;
-            } //else if (e_stat == v_end) 
-                //goto end_with_error;
-            break;                                  
-        case '}':
+            if (*(pos - 1) == '\\')
+                break;
+			if (e_stat == begin || e_stat == v_end) {
+				e_stat = k_begin_s_d;
+				k = pos+ 1;
+			} else if (e_stat == k_begin_s_d) {
+				e_stat = k_end;
+				k_len = pos - k;
+			} else if (e_stat == k_end) {
+				e_stat = v_begin_s_d;
+				v = pos + 1;
+			} else if (e_stat == v_begin_s_d) {
+				e_stat = v_end;
+				v_len = pos - v;
+			} 
+			break;
+			//
+        case '\'':
+			if (*(pos - 1) == '\\')
+				break;
+			if (e_stat == begin || e_stat == v_end) {
+				e_stat = k_begin_s_s;
+				k = pos + 1;
+			} else if (e_stat == k_begin_s_s) {
+				e_stat = k_end;
+				k_len = pos - k;
+			} else if (e_stat == k_end) {
+				e_stat = v_begin_s_s;
+				v = pos + 1;
+			} else if (e_stat == v_begin_s_s) {
+				e_stat = v_end;
+				v_len = pos - v;
+			}
+			break;
+			//
+        case '}': //????
             if (e_stat == v_end)
-                e_stat = done;
-            else if (e_stat == v_begin_n) {
-                v_len = pos - v;
-                e_stat = v_end;
-            }
+                e_stat = begin;
+			else if (e_stat == v_begin_n) {
+				e_stat = v_end;	
+				v_len = pos - v;
+			}
             break;                                  
         case ',':
             //{"a":123}
-            if (e_stat == v_begin_n) {
-                v_len = pos - v;
-                e_stat = v_end;
-            }
-            if (e_stat != v_end && e_stat != begin)
-                goto end_with_error;
+			if (e_stat == k_begin_n) {
+				e_stat = k_end;
+				k_len = pos - k;
+			} else if (e_stat == v_begin_n) {
+				e_stat = v_end;
+				v_len = pos - v;
+			} else if (e_stat == k_end) {
+				break;
+			} else if (e_stat == v_end) {
+				e_stat = begin;
+			}
             break;                                  
         case '[':
+		    //printf("==.\n");
             if (e_stat == k_end) {
             	r_bracket = find_right_bracket(pos, e - pos);
                 v = pos;
+				//printf("==%s.\n", v);
                 v_len = r_bracket + 1;
                 e_stat = v_end;
                 pos = v + v_len - 1;
             }
+			break;
         default:
-            if (e_stat == k_end) {
-                v = pos;
-                e_stat = v_begin_n;
-            }
+			if (e_stat == begin) {
+				e_stat = k_begin_n;
+				k = pos;
+			} else if (e_stat == k_end) {
+				e_stat = v_begin_n;
+				v = pos;
+			} else if (e_stat == v_end) {
+				e_stat = k_begin_n;
+				k = pos;
+			}
+			//
             break;
         } //end switch                                           
-        if (e_stat == done)
-            break;
-        else if (e_stat == v_end) {
+	    //printf("pos is %s.\n", pos);
+	    //printf("e_stat is %d.\n", e_stat);
+        if (e_stat == v_end) {
             printf("key: %.*s.\n", k_len, k);
             printf("value: %.*s.\n", v_len, v);
-            e_stat = begin;
+            //e_stat = begin;
             //插入arglist中
         }
     }                                               
+	return 0;
 
-    return 0;
 end_with_error:
+    printf("return with error, pos is %s\n", pos);
     return -1;
 }   
 
@@ -118,7 +162,7 @@ int find_right_bracket(char *pos, const size_t len)
     char *e = pos + len;
     char *s = pos;
     size_t i = 0, ret = 0;
-    //l 和 r分别记录左括号和右括号的个数
+    //l记录左括号个数
     size_t l = 0;
 
     if (*pos != '[')
@@ -145,7 +189,7 @@ int find_right_braces(char *pos, const size_t len)
     char *e = pos + len;
     char *s = pos;
     size_t i = 0, ret = 0;
-    //l 和 r分别记录左括号和右括号的个数
+    //l记录左括号个数
     size_t l = 0;
 
     if (*pos != '{')
@@ -169,11 +213,30 @@ int find_right_braces(char *pos, const size_t len)
 
 int main()
 {
-    //char *s = "{\"Name\":\"VIDEO\",\"type\":0,\"lastScene\":-1,\"ID\":5}";
-    char *s= "{\"e\" : [\"xiaoao\",\"xiaoba\",\"xiaoma\"],\"Education\":{\"GradeSchool\" : \"huayixiao\",\"MiddleSchool\" : [\"l\" , \"gao\"], \"University\" :{ \"Name\" : \"aa\", \"Specialty\" : [\"d\",\"aaa\"]}}}";
-    //char *s = "{\"N\":\"V\",\"t\":\"0\",\"lastScene\":\"-1\",\"ID\":\"5\"}";
-    //char *s = "{\"t\":0,\"l\":-1,\"ID\":5}";
-    printf(s);
-    http_json_arg_decode(s, s, strlen(s));
+    char *str[100];
+	int len = 0;
+    str[0] = "{\"Name\":\"VIDEO\",\"type\":0,\"lastScene\":-1,\"ID\":5}";
+    str[1] = "{\"e\" : [\"xa\",\"xb\"],\"Education\":{\"GradeSchool\" : \"huayixiao\",\"MiddleSchool\" : [\"l\" , \"gao\"], \"University\" :{ \"Name\" : \"aa\", \"Specialty\" : [\"d\",\"aaa\"]}}}";
+    str[2] = "{\"N\":\"V\",\"t\":\"0\",\"lastScene\":\"-1\",\"ID\":\"5\"}";
+    str[3] = "{\"t\":0,\"l\":-1,\"ID\":5}";
+	str[4] = "{\"admin\":{\"password\":\"0192023a\\\"7bbd732\\\"50516f069df18b500\",\"mail\":\"support@intra.nsfocus.com\",\"roles\":[\"ADMINISTRATORS\"],\"isSystem\":true,\"pswdLength\":8,\"pswdComplex\":{\"num\":3,\"lowLetter\":5,\"upLetter\":0,\"symbol\":0},\"alterDate\":1373864610},\"auditor\":{\"password\":\"5cb59fe845b83231b0e5aa95d96267e9\",\"mail\":\"support@intra.nsfocus.com\",\"roles\":[\"AUDITORS\"],\"isSystem\":true,\"pswdLength\":10,\"pswdComplex\":{\"num\":3,\"lowLetter\":7,\"upLetter\":0,\"symbol\":0},\"alterDate\":1373864943}}";
+	str[5] = "{\"BankId\":\"img=\\\"jav\\nascript:allert('xss')\"}";
+	str[6] = "{\"PayerAcNo\":\"6214692201000071363\",\"PayerBankAcType\":\"PDPB\",\"PayerAcName\":\".........\",\"DeptId\":\"801011\",\"PayerSubAc\":{\"Rate\":\"0.385\",\"SubDeptId\":\"801011\",\"TimRenewFlag\":\"0\",\"IntBeginDate\":\"2013-06-21\",\"Term\":\"000\",\"SubAcState\":\"0\",\"Currency\":\"CNY\",\"TimType\":\"S01\",\"SubOpenDate\":\"2012-02-17\",\"CRFlag\":\"X\",\"Balance\":\"12255.17\",\"SubAcSeq\":\"00001\",\"AvailBal\":\"12255.17\"},\"PayeeCifType\":\"2\",\"SysFlag\":\"1\",\"TransferType\":\"\",\"PayeeAcName\":\".........\",\"PayeeAcNo\":\"6216611900007541053\",\"SavePayeeFlag\":\"0\",\"RePayeeAcNo\":\"6216611900007541053\",\"NotifyPayeeFlag\":\"0\",\"PayeeMobile\":\"\",\"Priority\":\"0\",\"Amount\":\"20.00\",\"ChineseCash\":\"............\",\"Remark\":\"\",\"PayeeBankId\":\"104100000004\",\"ProvinceCode\":\"44\",\"CityCode\":\"5810\",\"UnionDeptId\":\"104581006015\",\"PayerBalance\":\"12255.17\",\"_SignTransaction\":\"Transfer\",\"displayFields\":\"PayerAcNo,PayeeAcNo,Amount\",\"PayerCurrency\":\"CNY\",\"PayerSubAcSeq\":\"00001\",\"CRFlag\":\"X\",\"PayeeBankName\":\"............\",\"ProvinceName\":\".........\",\"UnionDeptName\":\"................................................\",\"NoteCode\":\"EU\"}";
+	str[7] = "{\"Index\":\"RG\",\"Flag\":true}";
+	str[8] = "{'KeySN':'1AA3768C1AF3AA76B19F9415FD2B7B0D','ProjectID':'65','username':'root','password':'jl2013fq'}";
+	str[9] = "{header:{\"code\":0,\"message\":{\"title\":\"\",\"detail\":\"\"}},body:{dataStores:{\"inputData\":{rowSet:{\"primary\":[],\"filter\":[],\"delete\":[]},name:\"inputData\",pageNumber:1,pageSize:2147483647,recordCount:0}},parameters:{\"rid\":\"ee8c6829-0817-424a-ae2e-a9d356f4478e\",\"isDeleteMemo\":\"true\",\"reportId\":\"ee8c6829-0817-424a-ae2e-a9d356f4478e\"}}}";
+	str[10] = "{Index:RG,Flag:true}";
+	str[11] = "{\"admin\":{\"password\":\"0192023a\\\"7bbd732\\\"50516f069df18b500\",\"mail\":\"support@intra.nsfocus.com\",\"roles\":[\"ADMINISTRATORS\"],\"isSystem\":true,\"pswdLength\":8,\"pswdComplex\":{\"num\":3,\"lowLetter\":5,\"upLetter\":0,\"symbol\":0},\"alterDate\":1373864610}}";
+	str[12] = "testtest";
+	int i = 0;
+	len = 12;
+	printf("len is %d..\n", len);
+	for (; i < len; i++) {
+		//if (i != 11) continue;
+		printf("===========begin %d========.\n", i);
+    	printf("%s.\n", str[i]);
+    	http_json_arg_decode(str[i], str[i], strlen(str[i]));
+	}
+
     return 0;
 }
